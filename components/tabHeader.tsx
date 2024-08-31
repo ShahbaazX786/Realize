dc4a28c04390495f45108aa0600f3e5bb0154fc7
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-08-31 13:14:51 +0530

    Added a new compoennt tab header

---
+++ components/tabHeader.tsx
@@ -0,0 +1,25 @@
+import { AntDesign, Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
+
+const TabHeader = ({ title }: { title: string }) => {
+  const router = useRouter();
+
+  return (
+    <View className="h-14 shadow-md bg-white flex flex-row justify-between items-center px-4">
+      <View>
+        <Text className="text-xl font-semibold">{title}</Text>
+      </View>
+      <View className="flex flex-row justify-center items-center gap-5">
+        <TouchableOpacity onPress={() => router.replace("/premium")}>
+          <AntDesign name="staro" size={24} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => router.replace("/settings")}>
+          <Ionicons name="settings-outline" size={24} color="black" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default TabHeader;
