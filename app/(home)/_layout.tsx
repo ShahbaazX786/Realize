efd920b28cc51adebdaa98b0b7e5681a39c559bf
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-09-01 13:12:48 +0530

    changed background styles for layout

--- app/(home)/_layout.tsx
+++ app/(home)/_layout.tsx
@@ -3,7 +3,13 @@ import { Stack } from "expo-router";
 export default function HomeLayout() {
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="(tabs)"
+        options={{
+          headerShown: false,
+          contentStyle: { backgroundColor: "#f4f4f5" },
+        }}
+      />
     </Stack>
   );
 }
