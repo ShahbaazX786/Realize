df337793e35164deea4e5297f46e08b6b4f58fc7
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-09-01 13:05:36 +0530

    updated few styles

--- components/batteryCardNormal.tsx
+++ components/batteryCardNormal.tsx
@@ -10,12 +10,12 @@ const BatteryCardNormal = ({
   icon: any;
 }) => {
   return (
-    <View className="shadow-2xl rounded-xl flex flex-col justify-between bg-white p-6">
+    <View className="shadow-2xl rounded-2xl flex flex-col justify-between bg-white p-6">
       <View className="flex flex-row justify-between items-start w-full">
         <Text className="capitalize text-base text-gray-600">{title}</Text>
         <Text className="font-semibold text-lg">{value}</Text>
       </View>
-      <View className="mt-2">{icon}</View>
+      <View className="mt-10">{icon}</View>
     </View>
   );
 };
