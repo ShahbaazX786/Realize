24550015ae3aa9ccc5c0d0b91c2a8b0cd6a5c43f
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-09-01 12:48:35 +0530

    added 2 new cards

--- app/(home)/(tabs)/dashboard.tsx
+++ app/(home)/(tabs)/dashboard.tsx
@@ -1,6 +1,6 @@
 import BatteryCardNormal from "@/components/batteryCardNormal";
 import TabHeader from "@/components/tabHeader";
-import { Feather } from "@expo/vector-icons";
+import { AntDesign, Feather, FontAwesome6, Fontisto } from "@expo/vector-icons";
 import * as Battery from "expo-battery";
 import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
@@ -51,14 +51,31 @@ const Dashboard = () => {
 
   return (
     <SafeAreaView className="bg-white">
-      <View className="bg-gray-100">
-        <TabHeader title={"Dashboard"} />
-        <View className="my-2 mx-4 grid grid-cols-2">
+      <TabHeader title={"Dashboard"} />
+      <View className="bg-red-500 flex flex-col justify-center items-center px-3 py-3">
+        <View className="flex flex-row items-start justify-center">
           <BatteryCardNormal
             title={"Battery Health"}
             value={"Good"}
-            icon={<Feather name="battery" size={28} color="green" />}
+            icon={<AntDesign name="heart" size={24} color="green" />}
           />
+        </View>
+
+        <View className="flex flex-row items-start justify-center pt-3">
+          <View className="w-1/2 pr-2">
+            <BatteryCardNormal
+              title={"Status"}
+              value={"Charging"}
+              icon={<Fontisto name="battery-full" size={28} color="green" />}
+            />
+          </View>
+          <View className="w-1/2">
+            <BatteryCardNormal
+              title={"Charging Type"}
+              value={"AC"}
+              icon={<FontAwesome6 name="bolt" size={24} color="green" />}
+            />
+          </View>
         </View>
       </View>
     </SafeAreaView>
