492501d7ab8e6861fc929a77f1958df7be1f7ea9
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-09-04 19:01:25 +0530

    temporarily utilizing localdata.

--- app/(home)/(tabs)/charging.tsx
+++ app/(home)/(tabs)/charging.tsx
@@ -1,10 +1,38 @@
 import TabHeader from "@/components/tabHeader";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect, useState } from "react";
+import { FlatList, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Charging = () => {
+  const [deviceInfo, setDeviceInfo] = useState(null);
+
+  const getLocalData = async () => {
+    try {
+      const deviceInfo = await AsyncStorage.getItem("device_info");
+      return deviceInfo != null ? JSON.parse(deviceInfo) : null;
+    } catch (e) {
+      console.error("Unable to retrieve local data", e);
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getLocalData();
+      setDeviceInfo(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <SafeAreaView>
-      <TabHeader title={"Charging"} />
+      <ScrollView>
+        <TabHeader title={"Charging"} />
+        <View>
+          {deviceInfo?.features?.map((item, index) => <Text>{item}</Text>)}
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
