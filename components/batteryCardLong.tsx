f936047422828ae03880c5352d95beaf9ed18b0a
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-09-04 17:08:42 +0530

    minor style modifications

--- components/batteryCardLong.tsx
+++ components/batteryCardLong.tsx
@@ -13,7 +13,7 @@ const BatteryCardLong = ({
     <View className="shadow-2xl rounded-2xl flex flex-col justify-center items-center bg-white p-6">
       <View className="flex flex-col justify-center items-center w-full my-4">
         <Text className="capitalize text-lg text-gray-500">{title}</Text>
-        <Text className="font-semibold text-5xl text-center mt-6">
+        <Text className="font-semibold text-5xl text-center mt-4">
           {value}%
         </Text>
       </View>
