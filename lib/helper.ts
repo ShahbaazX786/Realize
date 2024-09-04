b3b2d4577a5aa6d2ad85e8b950d04ed4f94d2724
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-09-04 13:40:04 +0530

    Added helper functions for battery level, state.

--- lib/helper.ts
+++ lib/helper.ts
@@ -1,5 +1,25 @@
 import { urls } from "./constants";
 
-export const getUrl = (key: string) => {
+const getUrl = (key: string) => {
   return urls[key];
 };
+
+const getBatteryState = (key: number) => {
+  switch (key) {
+    case 1:
+      return "Discharging";
+    case 2:
+      return "Charging";
+    case 3:
+      return "Full";
+    default:
+      return "Unavailable";
+  }
+};
+
+const getBatteryLevel = (value: number) => {
+  const roundedValue = Math.round(value * 100) / 100;
+  return Math.ceil(roundedValue * 100);
+};
+
+export { getUrl, getBatteryState, getBatteryLevel };
